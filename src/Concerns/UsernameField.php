c778ea15acd50718448cae77189b7c55cb7e21a9
Hicham SADDEK <h.saddek@serrahub.com>
2021-03-29 10:40:54 +0200

    Adding UsernameField Scopes to the filters.
    Signed-off-by: Hicham SADDEK <h.saddek@serrahub.com>

---
+++ src/Concerns/UsernameField.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Milebits\Eloquent\Filters\Concerns;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use function Milebits\Helpers\Helpers\constVal;
+
+/**
+ * @mixin Model
+ */
+trait UsernameField
+{
+    public function bootUsernameField(): void
+    {
+
+    }
+
+    public function initializeUsernameField(): void
+    {
+        $this->mergeFillable([$this->getUsernameColumn()]);
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsernameColumn(): string
+    {
+        return constVal($this, 'USERNAME_COLUMN', 'username');
+    }
+
+    /**
+     * @param Builder $builder
+     * @param string $username
+     * @return Builder
+     */
+    public function scopeUsernameNot(Builder $builder, string $username): Builder
+    {
+        return $this->scopeUsername($builder, $username, '!=');
+    }
+
+    /**
+     * @param Builder $builder
+     * @param string $username
+     * @param string $operator
+     * @return Builder
+     */
+    public function scopeUsername(Builder $builder, string $username, string $operator = '='): Builder
+    {
+        return $builder->where($this->decideUsernameColumn($builder), $operator, $username);
+    }
+
+    /**
+     * @param Builder $builder
+     * @return string
+     */
+    public function decideUsernameColumn(Builder $builder): string
+    {
+        return count((property_exists($builder, 'joins') ? $builder->joins : [])) > 0
+            ? $this->getQualifiedUsernameColumn()
+            : $this->getUsernameColumn();
+    }
+
+    /**
+     * @return string
+     */
+    public function getQualifiedUsernameColumn(): string
+    {
+        return $this->qualifyColumn($this->getUsernameColumn());
+    }
+
+    /**
+     * @param Builder $builder
+     * @param string $username
+     * @return Builder
+     */
+    public function scopeUsernameLike(Builder $builder, string $username): Builder
+    {
+        return $this->scopeUsername($builder, Str::of($username)->append('%'), 'like');
+    }
+
+    /**
+     * @param Builder $builder
+     * @param string $username
+     * @return Builder
+     */
+    public function scopeUsernameNotLike(Builder $builder, string $username): Builder
+    {
+        return $this->scopeUsername($builder, Str::of($username)->append('%'), 'not like');
+    }
+}
