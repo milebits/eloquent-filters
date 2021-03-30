61d4e1ce7ba32b3538bbc367221e78f49529c9f6
Hicham SADDEK <h.saddek@serrahub.com>
2021-03-30 20:59:34 +0200

    Adding phone verification feature to the phone field
    Signed-off-by: Hicham SADDEK <h.saddek@serrahub.com>

--- src/Concerns/PhoneField.php
+++ src/Concerns/PhoneField.php
@@ -15,9 +15,23 @@ use function Milebits\Helpers\Helpers\constVal;
  */
 trait PhoneField
 {
+    public static function bootPhoneField(): void
+    {
+        self::registerPhoneVerificationScope();
+    }
+
+    public static function registerPhoneVerificationScope(): void
+    {
+        $verificationEnabled = constVal(self::class, 'PHONE_VERIFICATION', false);
+        if (!$verificationEnabled) $verificationEnabled = method_exists(self::class, 'phoneVerification') ? call_user_func([self::class, 'phoneVerification']) : $verificationEnabled;
+        if ($verificationEnabled) self::addGlobalScope('phoneVerification', fn($builder) => $builder->verifiedPhone());
+    }
+
     public function initializePhoneField(): void
     {
         $this->mergeFillable(Arr::wrap($this->getPhoneColumn()));
+        $this->mergeFillable(Arr::wrap($this->getPhoneVerifiedAtColumn()));
+        $this->mergeCasts([$this->getPhoneVerifiedAtColumn() => 'datetime']);
     }
 
     /**
@@ -86,5 +100,69 @@ trait PhoneField
     public function scopePhoneNotLike(Builder $builder, string $phone): Builder
     {
         return $this->scopePhone($builder, Str::of($phone)->append('%'), 'not like');
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhoneVerifiedAtColumn(): string
+    {
+        return constVal($this, 'PHONE_VERIFIED_AT_COLUMN', 'phone_verified_at');
+    }
+
+    /**
+     * @return string
+     */
+    public function getQualifiedPhoneVerifiedAtColumn(): string
+    {
+        return $this->qualifyColumn($this->getPhoneVerifiedAtColumn());
+    }
+
+    /**
+     * @param Builder $builder
+     * @return string
+     */
+    public function decidePhoneVerifiedAtColumn(Builder $builder): string
+    {
+        return count(property_exists($builder, 'joins') ? $builder->joins : []) > 0 ? $this->getQualifiedPhoneVerifiedAtColumn() : $this->getPhoneVerifiedAtColumn();
+    }
+
+    /**
+     * @param Builder $builder
+     * @param bool $verified
+     * @return Builder
+     */
+    public function scopeVerifiedPhone(Builder $builder, bool $verified = true): Builder
+    {
+        return $verified ? $builder->whereNotNull($this->decidePhoneVerifiedAtColumn($builder)) : $builder->whereNull($this->decidePhoneVerifiedAtColumn($builder));
+    }
+
+    /**
+     * @param Builder $builder
+     * @param bool $verified
+     * @return Builder
+     */
+    public function scopeUnverifiedPhone(Builder $builder, bool $verified = true): Builder
+    {
+        return $this->scopeVerifiedPhone($builder, !$verified);
+    }
+
+    /**
+     * @param bool $verify
+     * @return $this
+     */
+    public function verifyPhone(bool $verify = true): self
+    {
+        $this->{$this->getPhoneVerifiedAtColumn()} = $verify ? now() : null;
+        return $this;
+    }
+
+    /**
+     * @param bool $unVerify
+     * @return $this
+     */
+    public function unVerifyPhone(bool $unVerify = true): self
+    {
+        return $this->verifyPhone(!$unVerify);
     }
 }
