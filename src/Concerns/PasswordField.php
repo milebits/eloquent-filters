7bccc29deed0361088bad3b76648af1e2926cdca
Hicham SADDEK <h.saddek@serrahub.com>
2021-03-30 21:24:20 +0200

    Adding Password Field
    Signed-off-by: Hicham SADDEK <h.saddek@serrahub.com>

---
+++ src/Concerns/PasswordField.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace Milebits\Eloquent\Filters\Concerns;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use function Milebits\Helpers\Helpers\constVal;
+
+/**
+ * Trait PasswordField
+ * @package Milebits\Eloquent\Filters\Concerns
+ * @mixin Model
+ */
+trait PasswordField
+{
+    public function initializePasswordField(): void
+    {
+        $this->mergeFillable(Arr::wrap($this->getPasswordColumn()));
+        $this->makeHidden(Arr::wrap($this->getPasswordColumn()));
+    }
+
+    /**
+     * @return string
+     */
+    public function getPasswordColumn(): string
+    {
+        return constVal($this, 'PASSWORD_COLUMN', 'password');
+    }
+
+    /**
+     * @return string
+     */
+    public function getQualifiedPasswordColumn(): string
+    {
+        return $this->qualifyColumn($this->getPasswordColumn());
+    }
+
+    /**
+     * @param int $length
+     * @return string
+     */
+    public function generatePassword(int $length = 16): string
+    {
+        return Str::random($length);
+    }
+
+    /**
+     * @param string $password
+     * @return bool
+     */
+    public function checkPassword(string $password): bool
+    {
+        return Hash::check($this->{$this->getPasswordColumn()}, $password);
+    }
+}
