30492d615b37da76db71650926735596ba6f56d7
Hicham SADDEK <h.saddek@serrahub.com>
2021-06-13 19:44:40 +0200

    Adding new Filters
    Signed-off-by: Hicham SADDEK <h.saddek@serrahub.com>

---
+++ src/Filters/ActiveFilter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Milebits\Eloquent\Filters\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * Class ActiveFilter
+ * @package App\Filters
+ */
+class ActiveFilter extends ModelFilter
+{
+    /**
+     * The filter to apply to the model builder from the pipeline
+     *
+     * @param Builder $builder
+     * @return Builder
+     */
+    public function apply(Builder $builder): Builder
+    {
+        return $builder->where($this->key(), '=', (bool)$this->keyValue());
+    }
+}
---
+++ src/Filters/EnableFilter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Milebits\Eloquent\Filters\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * Class EnableFilter
+ * @package App\Filters
+ */
+class EnableFilter extends ModelFilter
+{
+    /**
+     * The filter to apply to the model builder from the pipeline
+     *
+     * @param Builder $builder
+     * @return Builder
+     */
+    public function apply(Builder $builder): Builder
+    {
+        return $builder->where($this->key(), '=', (bool)$this->keyValue());
+    }
+}
